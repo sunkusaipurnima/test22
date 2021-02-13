961228765cff1d5432e90859026a7170c2367555
sunkusaipurnima <saipurnima@gmail.com>
2021-02-13 05:58:27 +0530

    Added comment in BaseClass

--- BaseClass.js
+++ BaseClass.js
@@ -1,7 +1,8 @@
+// This is the parent class (class inheritance)
+
 class BaseClass{
     constructor(x, y, width, height, angle) {
         var options = {
-            'restitution':0.8,
             'friction':1.0,
             'density':1.0
         }
